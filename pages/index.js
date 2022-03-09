14700e5b745d995ca3408a1fb35f1a154978031e
Pei-Yao Hung <peiyaoh@gmail.com>
2022-03-09 13:15:35 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Walk To Joy
         </h1>
 
         <p className={styles.description}>

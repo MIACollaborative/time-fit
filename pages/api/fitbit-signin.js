f0692cca5dab7f7a6b0a4cce1e9d6b787cd8b762
Pei-Yao Hung <peiyaoh@gmail.com>
2022-03-10 01:53:29 -0500

    update

--- pages/api/fitbit-signin.js
+++ pages/api/fitbit-signin.js
@@ -8,21 +8,51 @@ export default async function handler(req, res) {
   console.log(`authCode: ${code}`);
   console.log(`state: ${state}`);
 
+  async function updateToken(hashCode, accessToken, refreshToken) {
+    console.log(`updateToken, hashCode: ${hashCode}`);
+    console.log(`updateToken, accessToken: ${accessToken}`);
+    console.log(`updateToken, refreshToken: ${refreshToken}`);
+    const firstUser = await prisma.users.findFirst({
+      where: { hash: hashCode },
+    });
+
+    console.log(`firstUser: ${JSON.stringify(firstUser)}`);
+
+    const updateUser = await prisma.users.update({
+      where: { username: firstUser.username },
+      data: {
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+      },
+    });
+
+    console.log(`updateUser: ${JSON.stringify(updateUser)}`);
+  }
+
   let authCode = code;
   let stateSplit = state.split("-");
   let hashCode = stateSplit[2];
 
+  console.log(`hash: ${hashCode}`);
+
   const user = await prisma.users.findFirst({
-    where: { hash: hashCode}
+    where: { hash: hashCode },
   });
 
-  return FitbitHelper.getAuthorizationInformation(authCode).then(
-    (responseData) => {
+  return FitbitHelper.getAuthorizationInformation(authCode)
+    .then((responseData) => {
       console.log(
         `FitbitHelper.getAuthorizationInformation: ${JSON.stringify(
           responseData
         )}`
       );
+
+      /*
+      if(responseData.status == 400){
+          // cannot auth: Bad Request
+          // I supposed this mean we need to authenticate again
+      }
+      */
 
       let accessToken = responseData.access_token;
 
@@ -30,15 +60,38 @@ export default async function handler(req, res) {
       let refreshToken = responseData.refresh_toekn;
 
       // To Do: ideally, store both
-      const updateUser = await prisma.user.update({
-        where: { hash: hashCode},
-        data: {
-            accessToken: accessToken,
-            refreshToken: refreshToken
-        },
-      })
+      updateToken(hashCode, accessToken, refreshToken);
 
-      return {username: user.username, accessToken, refreshToken };
-    }
-  );
+      res.status(200).json({ message: "authentication success" });
+      //return {message: "authentication success"}; //res.status(200).end();
+      //return {username: user.username, accessToken, refreshToken };
+    })
+    .catch((error) => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        //console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.statusText);
+        //console.log(error.response.headers);
+
+        console.log(`Error response`);
+        // which means, authentication falil
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+
+        console.log(`Error request`);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        // console.log('Error', error.message);
+
+        console.log("Error else");
+      }
+      res
+        .status(error.response.status)
+        .json({ message: "authentication failed" });
+    });
 }

cf4004aeab6750684e719c503536d4bfba5d9e68
Pei-Yao Hung <peiyaoh@gmail.com>
2022-03-15 15:57:41 -0400

    fix typo

--- pages/fitbit-signin.js
+++ pages/fitbit-signin.js
@@ -99,7 +99,8 @@ export async function getServerSideProps({ query }) {
     where: { hash: hashCode },
   });
 
-  let accessToken = responseData.access_token;
+  let accessToken = "";
+  let refreshToken = "";
 
   const authResult = await FitbitHelper.getAuthorizationInformation(authCode)
     .then((responseData) => {
@@ -119,7 +120,7 @@ export async function getServerSideProps({ query }) {
       accessToken = responseData.access_token;
 
       // If you followed the Authorization Code Flow, you were issued a refresh token. You can use your refresh token to get a new access token in case the one that you currently have has expired. Enter or paste your refresh token below. Also make sure you enteryour data in section 1 and 3 since it's used to refresh your access token.
-      let refreshToken = responseData.refresh_token;
+      refreshToken = responseData.refresh_token;
 
       // To Do: ideally, store both
       updateToken(hashCode, accessToken, refreshToken);

6f53e456dfcc9f46af676a73a6ce4185064f156f
Pei-Yao Hung <peiyaoh@gmail.com>
2022-03-16 16:28:38 -0400

    add debug msg

--- pages/main.js
+++ pages/main.js
@@ -66,7 +66,7 @@ export async function getServerSideProps(ctx) {
 
   const introspectResult = await FitbitHelper.introspectToken(user.accessToken)
       .then((responseData) => {
-        console.log(`FitbitHelper.introspectToken: ${JSON.stringify(responseData)}`);
+        console.log(`main.FitbitHelper.introspectToken: ${JSON.stringify(responseData)}`);
         //isAccessTokenActive = responseData.active;
         return responseData;
       })

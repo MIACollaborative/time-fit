db4026e12e16e69c2ceb82feee284da4090b445f
Pei-Yao Hung <peiyaoh@gmail.com>
2022-03-16 17:53:35 -0400

    add error handling

--- pages/main.js
+++ pages/main.js
@@ -81,7 +81,35 @@ export async function getServerSideProps(ctx) {
       //isAccessTokenActive = responseData.active;
       return responseData;
     })
-    .catch((error) => { return error; });
+    .catch((error) => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(`Data: ${error.response.data}`);
+        console.log(`Status: ${error.response.status}`);
+        console.log(`StatusText: ${error.response.statusText}`);
+        console.log(`Headers: ${error.response.headers}`);
+
+        console.log(`Error response`);
+        // which means, authentication falil
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+
+        console.log(`Error request`);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        // console.log('Error', error.message);
+
+        console.log("Error else");
+      }
+      //res.status(error.response.status).json({ response: inspect(error.response.data) });
+
+      let resultObj = eval(`(${inspect(error.response.data)})`);
+      return {value: "failed", data: resultObj};
+    });
 
 
     console.log(

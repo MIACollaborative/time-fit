a5ace51b1a32d0b0139509d47f43555be37197a2
Pei-Yao Hung <peiyaoh@gmail.com>
2022-03-17 00:58:45 -0400

    add debug msg

--- pages/activity-summary.js
+++ pages/activity-summary.js
@@ -217,6 +217,11 @@ export default function ActivitySummary({result, dateString}) {
               />:null
             }
         </div>
+        <div>
+            {
+                result.value == "failed" && resultData.errors[0]["errorType"] == "invalid_token"? <div>Please onnect your Fitbit in Settings.</div>:null
+            }
+        </div>
         <br />
         <br />
 

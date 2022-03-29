4611a9575513910f89e9c03503ca6f2f1b292b6d
Pei-Yao Hung <peiyaoh@gmail.com>
2022-03-28 22:33:38 -0400

    check weekday wakeup time and bed time, send out message.

--- test_script/test_cron_job.mjs
+++ test_script/test_cron_job.mjs
@@ -3,10 +3,11 @@ import axios from "axios";
 import { DateTime } from "luxon";
 
 // '*/2 * * * * *' -> every 2 seconds
+// '*/10 * * * * *' -> every 10 seconds
 // '* * * * *' -> every 1 minute
-nodeCron.schedule('*/10 * * * * *', async () => {
+nodeCron.schedule('* * * * *', async () => {
     console.log(`execute task every 10 seconds at ${DateTime.now()}`);
-    return axios({
+    let weekdayWakeupPromise = axios({
       method: 'post',
       url: 'http://localhost:3000/api/cron',
       // `headers` are custom headers to be sent
@@ -26,22 +27,29 @@ nodeCron.schedule('*/10 * * * * *', async () => {
       return data;
     });
 
-    const result = await fetch(`/api/cron?function_name=check_user_weekday_wakeup_time`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      /*
-      body: JSON.stringify({
-        type: type,
-        content: content
-      }),
-      */
-    }).then((r) => {
-      return r.json();
+    let weekdayBedPromise = axios({
+      method: 'post',
+      url: 'http://localhost:3000/api/cron',
+      // `headers` are custom headers to be sent
+      params: {
+        'function_name': 'check_user_weekday_bed_time',
+      }
+    })
+    .then((response) => {
+      //console.log(response.data);
+      console.log(response.status);
+      console.log(response.statusText);
+      //console.log(response.headers);
+      //console.log(response.config);
+
+      let data = response.data;
+      console.log(`Cron result: ${JSON.stringify(data)}`);
+      return data;
     });
-    console.log(`Cron result: ${JSON.stringify(result)}`);
-    return result;
+
+
+    return Promise.all([weekdayWakeupPromise, weekdayBedPromise]);
+
   });
 
 

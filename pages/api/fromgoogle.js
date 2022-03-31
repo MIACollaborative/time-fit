64d61c96370dca65fe09e90d1368739239bb378b
Pei-Yao Hung <peiyaoh@gmail.com>
2022-03-31 12:13:11 -0400

    add support in preparation for google sheet integration

--- pages/api/fromgoogle.js
+++ pages/api/fromgoogle.js
@@ -1,6 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import prisma from "../../lib/prisma";
 
-export default function handler(req, res) {
-    res.status(200).json({body: req.body})
+export default async function handler(req, res) {
+    console.log(`fromgoogle: ${JSON.stringify(req.body)}`);
+    const aResponse = await prisma.response.create({
+            data: {
+                content: req.body,
+            },
+        });
+
+    res.status(200).json({ result: aResponse });
+
 }
   

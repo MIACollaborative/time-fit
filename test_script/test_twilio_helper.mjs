40a4eedc80f88369e2b10115d9bdf3db1a2e4cfa
Pei-Yao Hung <peiyaoh@gmail.com>
2022-04-06 23:06:46 -0400

    Can log both twilio and noAction result

--- test_script/test_twilio_helper.mjs
+++ test_script/test_twilio_helper.mjs
@@ -1,8 +1,21 @@
 import * as dotenv from "dotenv";
 if (process.env.NODE_ENV !== "production") {
-    dotenv.config();
+  dotenv.config();
 }
 
 import TwilioHelper from "../lib/TwilioHelper.mjs";
 
-console.log(`TWILIO_ACCOUNT_SID: ${process.env.TWILIO_ACCOUNT_SID}, TWILIO_AUTH_TOKEN: ${process.env.TWILIO_AUTH_TOKEN}`);
+/*
+console.log(
+  `TWILIO_ACCOUNT_SID: ${process.env.TWILIO_ACCOUNT_SID}, TWILIO_AUTH_TOKEN: ${process.env.TWILIO_AUTH_TOKEN}`
+);
+*/
+
+/*
+TwilioHelper.sendMessage("7342773256", "test", []).then((tResult) => {
+  console.log(`TwilioHelper.sendMessage: ${JSON.stringify(tResult)}`);
+});
+*/
+
+let tResult = await TwilioHelper.sendMessage("7342773256", "test", []);
+console.log(`TwilioHelper.sendMessage: ${JSON.stringify(tResult)}`);

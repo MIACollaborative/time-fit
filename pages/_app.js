c36cbe4dd658a65485ac9a8c2a9d97495055419f
Pei-Yao Hung <peiyaoh@gmail.com>
2022-05-20 11:37:13 -0400

    use toast to provide feedback on msg send

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,16 @@ import { SessionProvider } from "next-auth/react";
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DateAdapter from '@mui/lab/AdapterLuxon';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <LocalizationProvider dateAdapter={DateAdapter}>
       <SessionProvider session={session}>
         <Component {...pageProps} />
+        <ToastContainer />
       </SessionProvider>
     </LocalizationProvider>
 

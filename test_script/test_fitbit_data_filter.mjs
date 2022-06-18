d9cdc43cccb313b5311190bafdea895f65641e9c
Pei-Yao Hung <peiyaoh@gmail.com>
2022-06-18 18:17:26 +0800

    add function for calculating average steps

--- test_script/test_fitbit_data_filter.mjs
+++ test_script/test_fitbit_data_filter.mjs
@@ -16,3 +16,10 @@ let fitbitRecordList = await DatabaseUtility.getUserFitbitActivityDataDuringPeri
 
 console.log(`fitbitRecordList: ${JSON.stringify(fitbitRecordList, null, 2)}`);
 console.log(`fitbitRecordList.length: ${fitbitRecordList.length}`);
+
+
+let average = await DatabaseUtility.getUserFitbitAverageDailyStepsDuringPeriodById("4SW9W9", "2022-06-06", "2022-06-12");
+
+
+console.log(`average: ${average}`);
+

86757ac9b3e6b790b52657696378b6ecbd607984
Pei-Yao Hung <peiyaoh@gmail.com>
2022-08-23 23:03:43 -0400

    Can generate username, pass, and hash

--- test_script/test_study_code_generator.mjs
+++ test_script/test_study_code_generator.mjs
@@ -2,8 +2,36 @@
 //import ServerService from "./utilities/ServerService";
 import { timer } from "rxjs";
 import { map, takeWhile} from "rxjs/operators";
-
+import prisma from "../lib/prisma.mjs";
 import cryptoRandomString from 'crypto-random-string';
+import csvWriter from "csv-write-stream";
+import fs from "fs";
+import md5 from "md5";
+import { DateTime } from "luxon";
+
+async function insertUser(newStudyCodeObj){
+  const {studyId, ...rest} = newStudyCodeObj;
+
+  await prisma.users.upsert({
+      where: {
+        username: newStudyCodeObj.username
+      },
+      update: {
+          ...rest
+      },
+      create: newStudyCodeObj,
+  })
+}
+
+async function writeToCSV(resultList, outputFileName){
+  var writer = csvWriter({ sendHeaders: true });
+  writer.pipe(fs.createWriteStream(outputFileName));
+  resultList.forEach((result) => {
+    writer.write(result);
+  });
+  writer.end();
+}
+
 
 /*
 for(let i = 17; i < 18; i++){
@@ -29,18 +57,52 @@ for(let i = 17; i < 18; i++){
 let initialDelay = 1000;
 let interval = 1000;
 let startIndex = 1;
-let endIndex = 51;
+let endIndex = 5;
 
-let prefix = `participant`;
+let prefix = `alpha`;
+
+let resultList = [];
+
+for(let i = startIndex; i < endIndex; i++){
+  let username = `${prefix}${i}`;
+    
+  //let hashStudyCode = md5(studyCode);
+  // cryptoRandomString({length: 10, type: 'alphanumeric'});
+  // let hashStudyCode = cryptoRandomString({length: 8, characters: 'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789'});
+  let password = cryptoRandomString({length: 8, characters: 'abcdefghijkmnpqrstuvwxyz023456789'});
+
+  let hash = md5(password);
+
+  console.log(`[${username}]: ${password}`);
+  let newStudyCodeObj = {
+    username,
+    password,
+    hash
+  };
+
+  resultList.push(newStudyCodeObj);
+
+}
+
+let dateString = DateTime.now().toISO({ format: 'basic', includeOffset: false });
+
+let exportFileName = `${prefix}_${dateString}.csv`;
+
+await writeToCSV(resultList, exportFileName);
+
+
 
 timer(initialDelay, interval).pipe(
   takeWhile(x => {
     return startIndex + x < endIndex;
   }),
   map(x => {
-    // pilot
-    //let studyCode = `pilotB${startIndex + x}`;
-    // study
+
+    let newStudyCodeObj = resultList[x];
+
+
+
+    /*
     let studyCode = `${prefix}${startIndex + x}`;
     
     //let hashStudyCode = md5(studyCode);
@@ -53,11 +115,16 @@ timer(initialDelay, interval).pipe(
       studyId: studyCode,
       passCode: hashStudyCode,
     };
+    */
 
     return newStudyCodeObj;
   })
 ).subscribe(newStudyCodeObj => {
   console.log(newStudyCodeObj);
+
+  insertUser(newStudyCodeObj);
+
+
 
   /*
   ServerService.submitOrReplaceInTable("study_code", newStudyCodeObj, false)

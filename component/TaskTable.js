d0d73e3d80392083d87bc081016512a80d60de2f
Pei-Yao Hung <peiyaoh@gmail.com>
2022-10-04 17:36:07 -0400

    initial draft of task table

---
+++ component/TaskTable.js
@@ -0,0 +1,146 @@
+
+/*
+import logger from "../lib/logger";
+
+*/
+
+import React, { useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import GeneralUtility from '../lib/GeneralUtility.mjs';
+
+
+function replacer(key, value) {
+  if (typeof value === "Date") {
+    return value.toString();
+  }
+  return value;
+}
+
+export default function TaskTable({ infoList }) {
+
+  /*
+  id  String  @id @default(auto()) @map("_id") @db.ObjectId
+
+  // for task
+  task    task    @relation(fields: [taskLabel], references: [label])
+  taskLabel String
+
+  // for user
+  user    users    @relation(fields: [username], references: [username])
+  username String
+
+  // caching user info
+  userInfoCache Json?
+
+  // for action
+  randomizationResult Json
+
+  // for message, possibly none if no action or action that does not invovle message
+  message    message?    @relation(fields: [messageLabel], references: [label])
+  messageLabel String?
+
+  
+  // for execution
+  executionResult Json?
+  
+  // for time
+  createdAt DateTime? @default(now())
+  updatedAt DateTime? @updatedAt
+  */
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Task Label</TableCell>
+            <TableCell align="right">Enabled</TableCell>
+            <TableCell align="right">Priority</TableCell>
+            <TableCell align="right">Participant Independent</TableCell>
+            <TableCell align="right">Ignore Timezone</TableCell>
+            <TableCell align="right">Check Point</TableCell>
+            <TableCell align="right">Group</TableCell>
+            <TableCell align="right">Randomization</TableCell>
+            <TableCell align="right">PreCondition</TableCell>
+            <TableCell align="right">Created At</TableCell>
+            <TableCell align="right">Updated At</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {infoList.map((row, index) => (
+            <TableRow
+              key={index}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">{row.taskLabel}</TableCell>
+              <TableCell align="right">{row.enabled}</TableCell>
+              <TableCell align="right">{row.priority}</TableCell>
+              <TableCell align="right">{row.participantIndependent}</TableCell>
+              <TableCell align="right">{row.ignoreTimezone}</TableCell>
+              <TableCell align="right">{JSON.stringify(row.checkPoint)}</TableCell>
+              <TableCell align="right">{JSON.stringify(row.group)}</TableCell>
+              <TableCell align="right">{JSON.stringify(row.randomization)}</TableCell>
+              <TableCell align="right">{JSON.stringify(row.preCondition)}</TableCell>
+              <TableCell align="right">{row.createdAt}</TableCell>
+              <TableCell align="right">{row.updatedAt}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+
+// extra
+
+/*
+const adminUsernameList = [
+  "test1",
+  "test2"
+];
+
+
+export async function getServerSideProps(ctx) {
+  
+  const session = await getSession(ctx);
+  console.log(
+    `main.getServerSideProps: session: ${JSON.stringify(session)}`
+  );
+
+  if(!session){
+    return {
+      props: {},
+    };
+  }
+
+  let userName = session.user.name;
+
+  
+  let responseList = [];
+  let responseInfoList = [];
+
+  if(adminUsernameList.includes(userName)){
+    responseList = await prisma.response.findMany({
+
+        orderBy: [
+            {
+              updatedAt: "desc",
+            },
+          ],
+        });
+      
+        responseInfoList = JSON.parse(JSON.stringify(responseList, replacer));
+    }
+    
+    return {
+      props: {responseInfoList},
+    };
+  }
+*/

99705419e6bb4706f0da10dbc9f73de3f5718117
Pei-Yao Hung <peiyaoh@gmail.com>
2022-10-11 12:38:41 -0400

    add time-zone page

---
+++ test_script/test_process_timezone_db.mjs
@@ -0,0 +1,132 @@
+//import md5 from "md5";
+//import ServerService from "./utilities/ServerService";
+import { timer } from "rxjs";
+import { map, takeWhile } from "rxjs/operators";
+import prisma from "../lib/prisma.mjs";
+import cryptoRandomString from 'crypto-random-string';
+import csvWriter from "csv-write-stream";
+import fs from "fs";
+import md5 from "md5";
+import { DateTime } from "luxon";
+import csv from "csv-parser";
+import path from "path";
+
+async function deleteAccountWithPrefix(prefix){
+  const deleteUsers = await prisma.users.deleteMany({
+    where: {
+      username: {
+        contains: prefix,
+      },
+    },
+  })
+}
+
+async function insertUser(newStudyCodeObj) {
+  const { username, ...rest } = newStudyCodeObj;
+
+
+  let existRecord = await prisma.users.findFirst({
+    where: {
+      username: username
+    }
+  });
+
+  console.log(`existRecord: ${existRecord}`);
+
+  if (existRecord == null) {
+    const user = await prisma.users.create({
+      data: newStudyCodeObj
+    })
+  }
+  else {
+
+    const updateUser = await prisma.users.update({
+      where: {
+        username: username
+      },
+      data: {
+        ...rest
+      },
+    })
+  }
+}
+
+async function insertUsers(results) {
+    // now insert into db using prisma
+  
+    const users = await prisma.users.createMany({
+      data: results,
+      //skipDuplicates: true, // Skip 'Bobo'
+    });
+  
+    return users;
+  }
+
+
+function timezoneNameToOffset(zoneName){
+    console.log(`timezoneNameToOffset: ${zoneName} -> ${DateTime.now().setZone(zoneName).offset}`);
+    return DateTime.now().setZone(zoneName).offset;
+}
+
+function readCSVAndInsert(fileName) {
+    console.log(`readCSVAndInsert.fileName: ${fileName}`);
+    let results = [];
+    fs.createReadStream(fileName)
+      .pipe(csv())
+      .on("data", (data) => {
+        results.push(data);
+        //console.log(`readCSVAndInsert.data(): ${JSON.stringify(data)}`);
+      })
+      .on("end", () => {
+        console.log(`readCSVAndInsert.end()`);
+
+        let usResults = [...new Set(results.filter((zoneInfo) => {
+            return zoneInfo["country_code"] == "US";
+        }).map((zoneInfo) => {
+            return zoneInfo["zone_name"];
+        })
+        
+        )];
+
+        console.log(usResults);
+
+        
+        usResults = usResults.map((zoneName) => {
+            let offset = timezoneNameToOffset(zoneName);
+
+            return {name: zoneName, offset: offset, offsetLabel: `GMT ${offset/60}`};
+        });
+        
+
+        console.log(usResults);
+
+        /*
+        let convertedResults = results.map((result) => {
+            return {...result, 
+                gif: result.gif.toLowerCase() == "true",
+                salience: result.salience.toLowerCase() == "true",
+                modification: result.modification.toLowerCase() == "true",
+            }
+        });
+        */
+
+        //insertUsers(convertedResults);
+
+        /*
+        results = results
+        .filter((obj) => {
+          return obj.group != "";
+        })
+        .map((obj) => {
+          const { group, id, ...rest } = obj;
+          let label = `${group}_${id}`;
+          return { label, group, groupIndex: Number(id), ...rest };
+        });
+        */
+
+      });
+  }
+
+let fileName = "content/db/time_zone.csv";
+
+readCSVAndInsert(fileName);

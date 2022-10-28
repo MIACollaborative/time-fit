713f114ef924c380d2643f24afb664d736cc94eb
Pei-Yao Hung <peiyaoh@gmail.com>
2022-10-28 16:48:35 -0400

    Create test_precondition_surveyFilledByThisPerson.mjs

---
+++ test_script/test_precondition_surveyFilledByThisPerson.mjs
@@ -0,0 +1,64 @@
+import DatabaseUtility from "../lib/DatabaseUtility.mjs";
+import prisma from "../lib/prisma.mjs";
+import TaskExecutor from "../lib/TaskExecutor.mjs";
+import {DateTime} from "luxon";
+
+let userInfo = await prisma.users.findFirst({
+    where: {
+        username: "test1"
+    }
+});
+
+
+let referenceDateStr1 = "joinAtDate";
+let referenceDateStr2 = "now";
+
+
+let sampleCondition =                     {
+    // Soo's Comment: This condition need to be modified to look for survey NOT filled out.
+    type: "surveyFilledByThisPerson",
+    criteria: {
+        // Id list: list of Qualtrics survey Ids to check
+        idList: ["SV_81aWO5sJPDhGZNA"], // baseline: SV_81aWO5sJPDhGZNA
+
+        // Whehter we want all ("and") surveys to be filled or at least one ("or") survey to be filled.
+        // Use ("not any") for checking survey NOT filled, etc.
+        idRelationship: "not any",
+        period: {
+            // Start: the starting piont of the time window to consider
+            // Removing it means we are consider a time window starting from the very beginning of time (year 200 for impelementation)
+            //start:{},
+            // reference:
+            // now: current time
+            // today: start of today (00:00:00 am)
+
+            // End: the end point of the time window to consider
+            // Removing it means we are consider a time window up to this point
+            end: {
+                // reference:
+                // now: current time
+                // today: end of today (23:59:59 pm)                                    
+                reference: "now",
+                // offset, the time that will be added ("plus") or substracted ("minus") from the reference
+                // Plus 0 hours basically means using the reference point directly
+                offset: { type: "plus", value: { hours: 0 } }
+            }
+        }
+    }
+};
+
+// checkOneConditionForUser(condition, userInfo, dateTime)
+
+
+//let testDate = DateTime.fromFormat("5/10/2022, 1:07:04 PM", "F");
+
+let testDate = DateTime.now();
+
+//let result = await TaskExecutor.checkOneConditionForUser(sampleCondition, userInfo, DateTime.utc());
+
+
+
+let result = await TaskExecutor.checkOneConditionForUser(sampleCondition, userInfo, testDate.toUTC());
+
+
+console.log(`checkOneConditionForUser: ${result}`);

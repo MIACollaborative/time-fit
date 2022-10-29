bd4122867505aa87223198afa560eef54402b7a0
Pei-Yao Hung <peiyaoh@gmail.com>
2022-10-28 22:59:22 -0400

    Update test_precondition_fitbitMinsDateRange.mjs

--- test_script/test_precondition_fitbitMinsDateRange.mjs
+++ test_script/test_precondition_fitbitMinsDateRange.mjs
@@ -3,45 +3,63 @@ import prisma from "../lib/prisma.mjs";
 import TaskExecutor from "../lib/TaskExecutor.mjs";
 import {DateTime} from "luxon";
 
+function replacer(key, value) {
+    if (typeof value === "Date") {
+        return value.toString();
+    }
+    return value;
+}
+
 let userInfo = await prisma.users.findFirst({
     where: {
         username: "test2"
     }
 });
 
+userInfo = JSON.parse(JSON.stringify(userInfo, replacer));
+
 
 let referenceDateStr = "today";
 
 
 let sampleCondition = {
-	
-	type: "hasHeartRateIntradayMinutesAboveThresholdForPersonByDateRange",
+    type: "hasHeartRateIntradayMinutesAboveThresholdForPersonByDate", // This type can only check the specified date inside the start: {}
+    opposite: false, // participant did adhere to wearing fitbit for +8 hours
+    criteria: {
+        idList: [""],
 
-	criteria: {
-        wearingLowerBoundMinutes: 30,
+        // Whehter we want all ("and") surveys to be filled or at least one ("or") survey to be filled.
+        // Use ("not any") for checking survey NOT filled, etc.
+        idRelationship: "and",
 
-        // should probably rename it, 
-        // "and" means, every day passes the threshold
-        // "or" means, at least one day passes the threshold
-        // "not any" means, none passes the threshold
+        // check whether minutes >= wearingLowerBoundMinutes
+        wearingLowerBoundMinutes: 60 * 8,
 
-        idRelationship: "and", 
-
-
-        period:{
-            start:{
-                reference: referenceDateStr, // "today"
-                offset: {type: "minus", value: {days: 15}}
-                
+        period: {
+            // Start: the starting piont of the time window to consider
+            // Removing it means we are consider a time window starting from the very beginning of time (year 200 for impelementation)
+            start: {
+                reference: "today",
+                offset: { type: "minus", value: { days: 1 } } // checks for wearing adherence 1 days ago (only that day)
             },
-            end:{			
-                reference: referenceDateStr, // "today"
-                offset: {type: "minus", value: {days: 0}},
-                
-            }
-        }
-	}
+            // reference:
+            // now: current time
+            // today: start of today (00:00:00 am)
 
+            // End doesn't matter for Fitbit wearing
+            // Removing it means we are consider a time window up to this point
+            // end:{
+            //     // reference:
+            //     // now: current time
+            //     // today: end of today (23:59:59 pm)
+            //     reference: "today",
+
+            //     // offset, the time that will be added ("plus") or substracted ("minus") from the reference
+            //     // Plus 0 hours basically means using the reference point directly
+            //     offset: {type: "minus", value: {days: 6}}
+            // }
+        }
+    }
 };
 
 // checkOneConditionForUser(condition, userInfo, dateTime)

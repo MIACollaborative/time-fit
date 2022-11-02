1458b858db6592af7d84544a01c7a2305432d695
Pei-Yao Hung <peiyaoh@gmail.com>
2022-11-02 10:26:04 -0400

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,14 +8,14 @@ export default NextAuth({
   providers: [
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
+      name: "ParticipantID",
       // The credentials is used to generate a suitable form on the sign in page.
       // You can specify whatever fields you are expecting to be submitted.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         username: {
-          label: "Username",
+          label: "ParticipantID",
           type: "text",
           placeholder: "Enter your participant ID",
         },

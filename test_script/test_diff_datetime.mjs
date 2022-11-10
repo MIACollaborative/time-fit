70b58c488d0845c4d157fee3357ce08b33f7b945
Pei-Yao Hung <peiyaoh@gmail.com>
2022-11-09 21:17:08 -0500

    update timestamp and also time comparison logic

---
+++ test_script/test_diff_datetime.mjs
@@ -0,0 +1,19 @@
+import * as dotenv from "dotenv";
+import { DateTime, Interval } from "luxon";
+import GeneralUtility from "../lib/GeneralUtility.mjs";
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+let now = DateTime.now().startOf("minute");
+
+console.log(`now: ${now}`);
+
+let before = now.minus({seconds: 69});
+let after = now.plus({seconds: 69});
+
+let unit = ["minutes", "seconds"];
+
+console.log(`diffDateTime(before:${before}, ${now}, ${unit}): ${JSON.stringify(GeneralUtility.diffDateTime(before, now, unit).toObject(), null, 2)}`);
+
+console.log(`diffDateTime(after:${after}, ${now}, ${unit}): ${JSON.stringify(GeneralUtility.diffDateTime(after, now, unit).toObject(), null, 2)}`);

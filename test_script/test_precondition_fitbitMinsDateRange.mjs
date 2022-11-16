5ec661d94e698ecb52b795e9efa608571875a1f5
Pei-Yao Hung <peiyaoh@gmail.com>
2022-11-16 12:18:01 -0500

    Update test_precondition_fitbitMinsDateRange.mjs

--- test_script/test_precondition_fitbitMinsDateRange.mjs
+++ test_script/test_precondition_fitbitMinsDateRange.mjs
@@ -111,6 +111,6 @@ let testDate = DateTime.fromFormat("11/04/2022, 08:00:00 AM", "F", { zone: userI
 //let result = await TaskExecutor.checkOneConditionForUser(sampleCondition, userInfo, DateTime.utc());
 
 let result = await TaskExecutor.checkOneConditionForUser(sampleCondition, userInfo, testDate);
-let result2 = await TaskExecutor.checkOneConditionForUser(sampleCondition2, userInfo, testDate);
+let result2 = await TaskExecutor.checkOneConditionForUser(sampleCondition2, userInfo, DateTime.now());
 
 console.log(`checkOneConditionForUser: ${result}, ${result2}`);

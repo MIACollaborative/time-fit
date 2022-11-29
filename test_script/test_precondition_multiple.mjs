d8e7e0320b0ed2c89eed519950cc0e25b882ddfc
Pei-Yao Hung <peiyaoh@gmail.com>
2022-11-29 17:25:16 -0500

    Update test_precondition_multiple.mjs

--- test_script/test_precondition_multiple.mjs
+++ test_script/test_precondition_multiple.mjs
@@ -140,7 +140,7 @@ let checkResultList = [];
 
 for (let i = 0; i < userInfoList.length; i++) {
     let userInfo = userInfoList[i];
-    let testDate = DateTime.fromFormat("11/23/2022, 12:00:00 PM", "F", { zone: userInfo.timezone });
+    let testDate = DateTime.fromFormat("11/22/2022, 12:00:00 PM", "F", { zone: userInfo.timezone });
     let checkResult = await TaskExecutor.isPreConditionMetForUser(sampleConditionObj, userInfo, testDate);
 
     checkResultList.push(checkResult);

46c5531a8bce1099dcb181663e87c8ba1859bdf8
Pei-Yao Hung <peiyaoh@gmail.com>
2022-11-30 10:19:09 -0500

    update fitbit update query

--- test_script/test_precondition_multiple.mjs
+++ test_script/test_precondition_multiple.mjs
@@ -36,73 +36,89 @@ let sampleConditionObj = {
     // whether a task has precondition to consider.
     enabled: true,
 
-    // Condition Relationship: deciding whether we need all conditions to be satisfied ("and"), or we need one of the condition to be satisfied ("or"), or we need none of the conditions to be satisfied ("not any").
-    conditionRelationship: "and",
+    // Condition Relationship: deciding whether we need all conditions to be satisfied ("and"), we need one of the condition to be satisfied ("or"), or we need none of the conditions to be satisfied ("not any").
+    conditionRelationship: "not any", // All conditions should return False
 
     // Condition list: list of conditions to be checked
     conditionList: [
         // Condition type: person, surveyFilledByThisPerson, timeInPeriod
         //See the checkOneConditionForUser() function in TaskExecutor.mjs for all the available condition type
-        /*
+
+        // Participants can be on either baseline or intervention to receive fitbit connection reminders
         {
-            // person -> check a participant's property
-            // For instance, the following example check whehter a person is in its baseline phase (with the "phase" property set to "baseline")
-            type: "person",
+            // type: "hasFitbitUpdateForPersonByDateRange" checks for fitbit update for the specified date
+            // Check if participant's Fitbit IS updating/syncing - should return False
+            type: "hasFitbitUpdateForPersonByDateRange",
+            opposite: false,
             criteria: {
-                phase: "baseline" // intervention +2days
-            }
-        },
-        */
-        {
-            type: "hasHeartRateIntradayMinutesAboveThresholdForPersonByDateRange", // This type can only check the specified date inside the start: {}
-            opposite: false, // participant did NOT adhere to wearing fitbit for +8 hours for 3 days = true
-            criteria: {
+                // Id list: list of Qualtrics survey Ids to check
                 idList: [""],
 
                 // Whehter we want all ("and") surveys to be filled or at least one ("or") survey to be filled.
                 // Use ("not any") for checking survey NOT filled, etc.
-                idRelationship: "not any", // used for hasHeartRateIntradayMinutesAboveThresholdForPersonByDateRange
-
-                // check whether minutes >= wearingLowerBoundMinutes
-                wearingLowerBoundMinutes: 60 * 8,
-                // REACTIVATE AFTER TESTING
-                // wearingDayLowerBoundCount: 3, // if specified, idRelationshi ignored; don't make it 0
-
-                // DELETE AFTER TESTING
-                wearingDayLowerBoundCount: 3,
-
+                idRelationship: "and",
                 period: {
                     // Start: the starting piont of the time window to consider
                     // Removing it means we are consider a time window starting from the very beginning of time (year 200 for impelementation)
-                    start: {
-                        // REACTIVATE AFTER TESTING
-                        // reference: "joinAtDate",
-                        // offset: { type: "minus", value: { days: 0 } } // checks for wearing adherence the last 6 days
-
-                        // DELETE AFTER TESTING
-                        reference: "today",
-                        offset: { type: "minus", value: { days: 3 } }
-                    },
                     // reference:
                     // now: current time
                     // today: start of today (00:00:00 am)
-
-                    // End doesn't matter for Fitbit wearing
+                    start: {
+                        reference: "today",
+                        offset: { type: "minus", value: { days: 2 } } // There was an update detected since 2 days ago - must return False
+                    },
+                    // End: the end point of the time window to consider
                     // Removing it means we are consider a time window up to this point
-                    // end:{
-                    //     // reference:
-                    //     // now: current time
-                    //     // today: end of today (23:59:59 pm)
-                    //     reference: "today",
+                    end: {
+                        // reference:
+                        // now: current time
+                        // today: end of today (23:59:59 pm)
+                        reference: "today",
 
-                    //     // offset, the time that will be added ("plus") or substracted ("minus") from the reference
-                    //     // Plus 0 hours basically means using the reference point directly
-                    //     offset: {type: "minus", value: {days: 6}}
-                    // }
+                        // offset, the time that will be added ("plus") or substracted ("minus") from the reference
+                        // Plus 0 hours basically means using the reference point directly
+                        offset: { type: "plus", value: { hours: 0 } }
+                    }
                 }
             }
         },
-        
+        {
+            // type: "hasFitbitUpdateForPersonByDateRange" checks for fitbit update for the specified date
+            // Check if participant's Fitbit isn't updating/syncing - should return True for reminder to stop at day 5.
+            type: "hasFitbitUpdateForPersonByDateRange",
+            opposite: true,
+            criteria: {
+                // Id list: list of Qualtrics survey Ids to check
+                idList: [""],
+
+                // Whehter we want all ("and") surveys to be filled or at least one ("or") survey to be filled.
+                // Use ("not any") for checking survey NOT filled, etc.
+                idRelationship: "and",
+                period: {
+                    // Start: the starting piont of the time window to consider
+                    // Removing it means we are consider a time window starting from the very beginning of time (year 200 for impelementation)
+                    // reference:
+                    // now: current time
+                    // today: start of today (00:00:00 am)
+                    start: {
+                        reference: "today",
+                        offset: { type: "minus", value: { days: 5 } } // There wasn't an update detected since 5 days ago
+                    },
+                    // End: the end point of the time window to consider
+                    // Removing it means we are consider a time window up to this point
+                    end: {
+                        // reference:
+                        // now: current time
+                        // today: end of today (23:59:59 pm)
+                        reference: "today",
+
+                        // offset, the time that will be added ("plus") or substracted ("minus") from the reference
+                        // Plus 0 hours basically means using the reference point directly
+                        offset: { type: "plus", value: { hours: 0 } }
+                    }
+                }
+            }
+        },
     ]
 };
 

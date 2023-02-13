7bfea773c30bbff51e604ba1e1bbf1f68fa06643
Pei-Yao Hung <peiyaoh@gmail.com>
2023-02-13 14:51:05 -0500

    Update test_message_placeholder.mjs

--- test_script/test_message_placeholder.mjs
+++ test_script/test_message_placeholder.mjs
@@ -21,7 +21,7 @@ let userInfo = JSON.parse(JSON.stringify(aUser, replacer));
 // let sampleMesssageTemplate = `Your last response: [response|SV_73Dz01KrwwLUyk6:SV_bw498iRdfDhdLme|last].`;
 
 // template:[survey_link_from_tasks|SV_bBoOhje0dSNbZgq:SV_cACIS909SMXMUp8|last]
-let sampleMesssageTemplate = `Your last survey link: [survey_link_from_tasks|SV_73Dz01KrwwLUyk6:SV_bw498iRdfDhdLme|last].`;
+let sampleMesssageTemplate = `Your last survey link: [survey_link_from_tasks|intervention_planning_1:intervention_planning_2-6|last].`;
 
 
 let resultMsg = await DatabaseUtility.replacePlaceholderFromMessage(sampleMesssageTemplate, userInfo, "");

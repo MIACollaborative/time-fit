dc8bcb1a246f8540a71aa530b4014b9038a5d6b3
Pei-Yao Hung <peiyaoh@gmail.com>
2023-02-22 11:17:17 -0500

    update way to get local time

---
+++ test_script/test_time_creation.mjs
@@ -0,0 +1,30 @@
+import { DateTime } from "luxon";
+import DatabaseUtility from "../lib/DatabaseUtility.mjs";
+import GeneralUtility from "../lib/GeneralUtility.mjs";
+import prisma from "../lib/prisma.mjs";
+
+
+let aUser = await prisma.users.findFirst({
+    where: {
+        username: "test1"
+    }
+});
+
+
+function replacer(key, value) {
+    if (typeof value === "Date") {
+        return value.toString();
+    }
+    return value;
+}
+
+let userInfo = JSON.parse(JSON.stringify(aUser, replacer));
+
+//console.log(`userInfo: ${JSON.stringify(userInfo)}`);
+
+let dateString = "2023-02-22";
+
+
+let targetDate = DateTime.fromISO(dateString, {setZone: true}, {zone: userInfo.timezone} );
+
+console.log(`targetDate: [${targetDate}]--------------`);

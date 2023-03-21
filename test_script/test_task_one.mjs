5d44d79f9f5105bc941b4b510954adf844bc8e0f
Pei-Yao Hung <peiyaoh@gmail.com>
2023-03-21 17:14:48 -0400

    Update test_task_one.mjs

--- test_script/test_task_one.mjs
+++ test_script/test_task_one.mjs
@@ -22,23 +22,14 @@ function replacer(key, value) {
 let userList = await prisma.users.findMany({
     where: {
         username: {
-          contains: "test1",
+          contains: "test2",
         },
       },
 });
 
 let userInfoList = JSON.parse(JSON.stringify(userList, replacer));
 
-
-/*
-const user = await prisma.users.findFirst({
-    where:{
-        username: "test1"
-    }
-})
-
-let userInfo = JSON.parse(JSON.stringify(user, replacer));
-*/
+let oneUser = userInfoList[0];
 
 // 2022.08.31 02:00 PM (EDT)
 //let dateTime = DateTime.fromJSDate(new Date(2022, 7, 31, 14, 0, 0));
@@ -46,14 +37,22 @@ let userInfo = JSON.parse(JSON.stringify(user, replacer));
 // let's create one task that has all the conditions
 
 let taskList = GeneralUtility.taskList.filter((taskInfo) => {
-    return taskInfo.label == "fitbit process notification";
+    return taskInfo.label == "intervention_morning gif";
 });
 
 
 for(let i = 0; i < taskList.length; i++){
     let oneTask = taskList[i];
-    let testDate = DateTime.fromFormat("11/30/2022, 09:00:00 AM", "F", { zone: "America/Detroit" });
-    let result = await TaskExecutor.executeTaskForUserListForDatetime(oneTask, [GeneralUtility.systemUser], testDate);
-    console.log(`result: ${JSON.stringify(result, null, 2)}`);
+
+    let startDate = DateTime.fromFormat("03/13/2023, 12:00:00 PM", "F", { zone: "America/Detroit" });
+    
+    // now, go through a week
+    for(let j = 0; j <=7; j++){
+        let curDate = startDate.plus({days: j});
+        let result = await TaskExecutor.executeTaskForUserListForDatetime(oneTask, [oneUser], curDate);
+        console.log(`[${curDate}] result: ${JSON.stringify(result, null, 2)}\n\n`);
+    }
+
+    
 }
 

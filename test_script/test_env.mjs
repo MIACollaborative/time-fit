877c3647a4cdb808075d8cf538f7396ce243e9bc
Pei-Yao Hung <peiyaoh@gmail.com>
2023-03-21 17:34:14 -0400

    Create test_env.mjs

---
+++ test_script/test_env.mjs
@@ -0,0 +1,14 @@
+import * as dotenv from "dotenv";
+import { DateTime } from "luxon";
+import prisma from "../lib/prisma.mjs";
+import TaskExecutor from "../lib/TaskExecutor.mjs";
+import DatabaseUtility from "../lib/DatabaseUtility.mjs";
+import GeneralUtility from "../lib/GeneralUtility.mjs";
+
+
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
+
+console.log(`NEXTAUTH_URL: ${process.env.NEXTAUTH_URL}`);
+console.log(`DATABASE_URL: ${process.env.DATABASE_URL}`);

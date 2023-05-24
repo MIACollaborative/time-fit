7c990c8c77fa0f100691d6f7a6607516f7888e58
Pei-Yao Hung <peiyaoh@gmail.com>
2023-05-24 12:57:02 -0400

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -449,7 +449,7 @@ export default function Dashboard({ responseInfoList, fitbitSubscriptionInfoList
         },
         body: JSON.stringify({
           limit: 0,
-          startDate: nowDate.minus({weeks: 1}).startOf("day"), // startDate.toISO(),
+          startDate: nowDate.minus({days: 4}).startOf("day"), // startDate.toISO(),
           endDate: nowDate.toISO()
         }),
       })

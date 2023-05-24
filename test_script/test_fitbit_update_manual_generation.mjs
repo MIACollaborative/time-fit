27c33cc4dc7be5a9f8e3eb94f22e37e40a787a58
Pei-Yao Hung <peiyaoh@gmail.com>
2023-05-24 16:18:54 -0400

    Update test_fitbit_update_manual_generation.mjs

--- test_script/test_fitbit_update_manual_generation.mjs
+++ test_script/test_fitbit_update_manual_generation.mjs
@@ -93,5 +93,5 @@ console.log(`[Summary] ----------------------------------------`);
 for(let i = 0; i < summaryList.length; i++){
     let sInfo = summaryList[i];
 
-    console.log(sInfo.username, sInfo.datetime.toLocal(), sInfo.updateGenerated);
+    console.log(sInfo.username, DateTime.fromISO(sInfo.datetime).toLocaleString(DateTime.DATETIME_FULL), sInfo.updateGenerated);
 }

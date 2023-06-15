dc397ee79e889f13d6451f26e0a8880f81028bfb
Pei-Yao Hung <peiyaoh@gmail.com>
2023-06-15 15:01:22 -0400

    fix bug

--- pages/api/update-diff.js
+++ pages/api/update-diff.js
@@ -23,7 +23,8 @@ export default async function handler(req, res) {
     console.log(`function: ${function_name}`);
 
 
-    const { username } = req.body;
+    let username = session.user.name;
+    //const { username } = req.body;
 
     switch (function_name) {
         case "get":

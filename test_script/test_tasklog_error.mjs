eb8aa0b289b8e01db9e09dc69e212686fefd6748
Pei-Yao Hung <peiyaoh@gmail.com>
2023-07-25 22:32:26 -0400

    Create test_tasklog_error.mjs

---
+++ test_script/test_tasklog_error.mjs
@@ -0,0 +1,43 @@
+import * as dotenv from "dotenv";
+import { DateTime } from "luxon";
+import prisma from "../lib/prisma.mjs";
+import TaskExecutor from "../lib/TaskExecutor.mjs";
+import DatabaseUtility from "../lib/DatabaseUtility.mjs";
+import v from "voca";
+//import { DateTime } from "luxon";
+
+
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
+
+
+
+
+let username = "test1";
+
+// this gives you the number of taskLog that associated with this particular user.
+
+const taskLogFailedList = await prisma.taskLog.findMany({
+    where: {
+        taskLabel: "fitbit process notification",
+        executionResult: {
+            type: "fitbit-process-update",
+            value: {
+                status: "failed"
+            }
+        },
+    }
+})
+
+
+let failedActionResultList = taskLogFailedList.map((taskLog) => {
+    return JSON.parse(v.replaceAll(taskLog.executionResult.value.errorMessage, "\n", ""));
+})
+
+
+
+
+
+
+console.log(`failedActionResultList: ${JSON.stringify(failedActionResultList, null, 2)}`);

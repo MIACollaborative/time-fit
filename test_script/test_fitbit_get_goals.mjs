4df14c15e590ea19e2f37f7da23f35c4a44b2ff8
Pei-Yao Hung <peiyaoh@gmail.com>
2024-11-26 11:30:28 -0500

    test script for get goal

--- test_script/test_fitbit_get_goals.mjs
+++ test_script/test_fitbit_get_goals.mjs
@@ -1,51 +1,19 @@
 import FitbitHelper from "../lib/FitbitHelper.mjs";
 import { inspect } from 'util';
-//import MongoDBHelper from "../utilities/MongoDBHelper.mjs"
-//import * as dotenv from "dotenv";
+import * as dotenv from "dotenv";
 
-/*
 if (process.env.NODE_ENV !== "production") {
     dotenv.config();
 }
-*/
 
-//const mHelper = new MongoDBHelper();
-
-//const databaseName = "walk_to_joy";
-
-//mHelper.testConnection();
-
-
-// MongoDBHelper.insertDataIntoTable("walk_to_joy", "logs", [{message: "test3"}], true);
-
-/*
-FitbitHelper.getProfile(`${process.env.FITBIT_ACCESS_TOKEN}`)
-.then((responseData) => {
-    console.log(`FitbitHelper.getProfile: responseData`);
-
-    // console.log(`FitbitHelper.getProfile: responseData ${JSON.stringify(responseData)}`);
-});
-*/
-
-/*
-FitbitHelper.getAuthorizationInformation("***REMOVED***07")
-.then((responseData) => {
-
-    console.log(
-        `FitbitHelper.getAuthorizationInformation: responseData: ${responseData}`
-      );
-});
-*/
-
+let fitbitID = process.env.FITBIT_TEST_ID;
 let accessToken = process.env.FITBIT_TEST_ACCESS_TOKEN;
 let testToken = accessToken;
 
-
-
-FitbitHelper.introspectToken(accessToken, testToken)
+FitbitHelper.getActivityGoalsForFitbitID(fitbitID, accessToken, "daily")
 .then((responseData) => {
   console.log(
-      `FitbitHelper.introspectToken: responseData: ${responseData}`
+      `FitbitHelper.getActivityGoalsForFitbitID: responseData: ${responseData}`
     );
 })
 .catch((error) => {

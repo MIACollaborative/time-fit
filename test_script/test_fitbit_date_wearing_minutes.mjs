29b6a6fb62ad1e702fe7d8c6e2a73f37426cb03c
Pei-Yao Hung <peiyaoh@gmail.com>
2024-12-02 15:05:04 -0500

    add date and minues extract facility

---
+++ test_script/test_fitbit_date_wearing_minutes.mjs
@@ -0,0 +1,26 @@
+import * as dotenv from "dotenv";
+import prisma from "../lib/prisma.mjs";
+import DatabaseUtility from "../lib/DatabaseUtility.mjs";
+import { DateTime } from "luxon";
+
+
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
+
+let userList = await prisma.users.findMany({
+    where: {
+        username: {
+          contains: "test1",
+        },
+      },
+});
+
+
+let theUser = userList[0];
+
+console.log(JSON.stringify(theUser, null, 2));
+
+
+
+const dateGoalList = await DatabaseUtility.getUserFitbitDateAndWearingMinutesListDuringPeriod(theUser.fitbitId, DateTime.fromISO("2024-11-10"), DateTime.fromISO("2024-12-02"));

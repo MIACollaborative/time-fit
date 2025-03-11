ec41d5154557ded81955b27d59851b1a83a98f5b
Pei-Yao Hung <peiyaoh@gmail.com>
2025-03-11 12:27:55 -0400

    add structure in comment

--- src/utility/prisma.mjs
+++ src/utility/prisma.mjs
@@ -1,15 +1,4 @@
-//import { PrismaClient } from "@prisma/client";
-
-
-// version 2
-
-
 import { PrismaClient } from '@prisma/client'
-
-/*
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
-*/
 
 const globalForPrisma = globalThis;
 
@@ -18,13 +7,3 @@ export const prisma =
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 export default prisma;
-
-
-// version 1
-/*
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
-
-const prisma = new PrismaClient();
-export default prisma;
-*/

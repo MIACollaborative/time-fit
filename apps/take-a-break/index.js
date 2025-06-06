ee15baf8d4e52d212768b0c0071c9f731f2f2c7f
Pei-Yao Hung <peiyaoh@gmail.com>
2025-06-06 17:17:46 -0400

    ok, take-a-break works

--- apps/take-a-break/index.js
+++ apps/take-a-break/index.js
@@ -1,5 +1,5 @@
-import TimeEngine from "../../framework/time-engine/TimeEngine.js";
-import DesktopNotificationAction from "../../framework/action-collection/DesktopNotificationAction.js";
+import TimeEngine from "@time-fit/time-engine/TimeEngine.js";
+import DesktopNotificationAction from "@time-fit/action-collection/DesktopNotificationAction.js";
 
 // create a register an action
 const newAction = new DesktopNotificationAction(

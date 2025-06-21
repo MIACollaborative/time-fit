45e021c053b7005d76cc2ffc3fa3439c65f83cf9
Pei-Yao Hung <peiyaoh@gmail.com>
2025-06-20 22:31:26 -0400

    Add Mailjet email action and helper classes
    Introduces MailjetEmailAction for sending emails via Mailjet and MailjetHelper to handle the actual email sending logic using the node-mailjet library. These additions enable email sending functionality within the application.

---
+++ packages/action-collection/MailjetEmailAction.js
@@ -0,0 +1,22 @@
+import MailjetHelper from "@time-fit/helper/MailjetHelper";
+
+export default class MailjetEmailAction {
+    #emailInfo;
+
+  constructor(emailInfo) {
+    this.#emailInfo = emailInfo;
+  }
+
+  async execute(actionInfo, params) {
+    const { userInfo, datetime } = params;
+
+    const emailInfo = actionInfo.emailInfo? actionInfo.emailInfo : this.#emailInfo;
+
+    const result = await MailjetHelper.sendEmail([emailInfo]);
+
+    return {
+      type: "mailjet-email",
+      value: result,
+    };
+  }
+}
